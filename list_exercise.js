851f47dc0cee14ea8d9697f91dede1eef9afa3e2
abdellah elhabchi <50263980+ael-habc@users.noreply.github.com>
2023-01-01 16:36:15 +0100

    exercise js

---
+++ list_exercise.js
@@ -0,0 +1,129 @@
+//Conditional exercise
+
+/*
+
+1. Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign. Go to the editor
+Sample numbers : 3, -7, 2
+Output : The sign is -
+
+*/
+
+
+
+
+
+// String Exercise
+
+/*
+
+1. Write a JavaScript function to check whether an `input` is a string or not. 
+Test Data :
+console.log(is_string('w3resource'));
+true
+console.log(is_string([1, 2, 4, 0]));
+false
+
+2. Write a JavaScript function to check whether a string is blank or not. 
+Test Data :
+console.log(is_Blank(''));
+console.log(is_Blank('abc'));
+true
+false
+
+3. Write a JavaScript function to split a string and convert it into an array of words. 
+Test Data :
+console.log(string_to_array("Robin Singh"));
+["Robin", "Singh"]
+
+4. Write a JavaScript function to extract a specified number of characters from a string. 
+Test Data :
+console.log(truncate_string("Robin Singh",4));
+"Robi".
+
+5. Write a JavaScript function to convert a string in abbreviated form. 
+Test Data :
+console.log(abbrev_name("Robin Singh"));
+"Robin S.".
+
+6. Write a JavaScript function to hide email addresses to protect from unauthorized user. 
+Test Data :
+console.log(protect_email("robin_singh@example.com"));
+"robin...@example.com".
+
+7. Write a JavaScript function to parameterize a string. 
+Test Data :
+console.log(string_parameterize("Robin Singh from USA."));
+"robin-singh-from-usa".
+
+8. Write a JavaScript function to capitalize the first letter of a string. 
+Test Data :
+console.log(capitalize('js string exercises'));
+"Js string exercises".
+
+9. Write a JavaScript function to capitalize the first letter of each word in a string. 
+Test Data :
+console.log(capitalize_Words('js string exercises'));
+"Js String Exercises".
+
+10. Write a JavaScript function that takes a string which has lower and upper case letters as a parameter and converts upper case letters to lower case, and lower case letters to upper case. 
+Test Data :
+console.log(swapcase('AaBbc'));
+"aAbBC"
+
+*/
+
+//Array Exercise
+
+
+/* 
+
+1. Write a JavaScript function to check whether an `input` is an array or not.
+Test Data :
+console.log(is_array('w3resource'));
+false
+console.log(is_array([1, 2, 4, 0]));
+true
+
+2. Write a JavaScript function to convert an amount to coins. Go to the editor
+Sample function : amountTocoins(46, [25, 10, 5, 2, 1])
+Here 46 is the amount. and 25, 10, 5, 2, 1 are coins.
+Output : 25, 10, 10, 1
+
+3. Write a JavaScript function to generate an array between two integers of 1 step length.
+Sample function : rangeBetween(4, 7)
+Expected Output : [4, 5, 6, 7]
+Sample function :  rangeBetween(-4, 7)
+Expected Output : [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+
+4. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+Test Data :
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+Expected Output :
+7
+[]
+[7, 9, 0]
+[7, 9, 0, -2]
+[]
+
+5. Write a simple JavaScript program to join all elements of the following array into a string (with spaces).
+Sample array : myColor = ["Red", "Green", "White", "Black"];
+Expected Output :
+"Red Green White Black"
+"Red,Green,White,Black"
+"Red+Green+White+Black"
+
+6. Write a JavaScript program to find the most frequent item of an array.
+Sample array : var arr1= [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+Sample Output : a ( 5 times )
+
+7. Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.
+
+8. Write a JavaScript program to sort the items of an array.
+Sample array : var arr1= [3, 8, 7, 6, 5, -4, 3, 2, 1];
+Sample Output : -4,-3,1,2,3,5,6,7,8
+
+*/
--- tp.js
+++ tp.js
@@ -1,20 +1,20 @@
-function aime(arr)
-{
-    if (arr.length == 0)
-        return "personne n'aime ça"
-    else if (arr.length == 1)
-        return `${arr[0]} aime ça`
-    else if (arr.length == 2)
-        return `${arr[0]} et ${arr[1]} aiment ça`
-    else if (arr.length == 3)
-        return `${arr[0]},${arr[1]} et ${arr[2]} aiment ça`
-    else
-        return `${arr[0]}, ${arr[1]} et ${arr.length - 2}  autres comme ça`
-}
+// function aime(arr)
+// {
+//     if (arr.length == 0)
+//         return "personne n'aime ça"
+//     else if (arr.length == 1)
+//         return `${arr[0]} aime ça`
+//     else if (arr.length == 2)
+//         return `${arr[0]} et ${arr[1]} aiment ça`
+//     else if (arr.length == 3)
+//         return `${arr[0]},${arr[1]} et ${arr[2]} aiment ça`
+//     else
+//         return `${arr[0]}, ${arr[1]} et ${arr.length - 2}  autres comme ça`
+// }
 
-// console.log(aime([]))
+// // console.log(aime([]))
 
-function shiftZero(arr)
-{
+// function shiftZero(arr)
+// {
     
-}
+// }
